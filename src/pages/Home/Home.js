048e9833f43c1d598c783feeb85e3bf7671c62d3
bphil1041 <benjamin.r.phillips1041@gmail.com>
2024-03-05 21:42:10 -0600

    Create Home.js

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+
+function Home() {
+    return (
+        <div>
+            <h1>Ben Phillips</h1>
+            <p>Web Developer / Musician</p>
+        </div>
+    );
+}
+
+export default Home;
