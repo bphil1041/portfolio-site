b278e79955af0cf0ddaa31e69a762d06fa84e5de
bphil1041 <benjamin.r.phillips1041@gmail.com>
2024-03-05 21:25:08 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,48 +1,23 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
-
-function Home() {
-  return (
-    <div>
-      <h2>Home Page</h2>
-      <p>Welcome to the Home Page!</p>
-    </div>
-  );
-}
-
-function About() {
-  return (
-    <div>
-      <h2>About Page</h2>
-      <p>Welcome to the About Page!</p>
-    </div>
-  );
-}
+import NavBar from './components/NavBar/NavBar';
+import Home from './pages/Home';
+import About from './pages/About';
+import Projects from './pages/Projects';
+import Contact from './pages/Contact';
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-          </ul>
-        </nav>
-
-        <Switch>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
+        <NavBar />
+        <Routes>
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
       </div>
     </Router>
   );
