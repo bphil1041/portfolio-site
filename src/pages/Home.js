f4f50fc244996dae48a9cb61e4cf762daf68cb08
bphil1041 <benjamin.r.phillips1041@gmail.com>
2024-03-05 21:25:22 -0600

    Update Home.js

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+    return (
+        <div>
+            <h1>Ben Phillips</h1>
+            <p>Web Developer</p>
+        </div>
+    );
+}
+
+export default Home;
