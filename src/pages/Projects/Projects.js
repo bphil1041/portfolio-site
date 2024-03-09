d9847bc2cfd523e65221fce8a1be9428812a56e4
bphil1041 <benjamin.r.phillips1041@gmail.com>
2024-03-09 08:48:56 -0600

    so many changes

--- src/pages/Projects/Projects.js
+++ src/pages/Projects/Projects.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Card, Col, Row, Modal, Button } from 'react-bootstrap';
-import './Projects.css'; // Assuming you have a Projects.css file for styling
-import project1Image from '../../assets/Screenshot 2024-03-05 at 11.46.28 PM 2.png'
+import './Projects.css';
+import project1Image from '../../assets/Screenshot 2024-03-05 at 11.46.28 PM 2.png';
+import project2Image from '../../assets/myFlixScreenshot.png';
 
 function Projects() {
     const [showModal, setShowModal] = React.useState(false);
@@ -9,8 +10,8 @@ function Projects() {
 
     const projects = [
         { id: 1, name: 'JavaScript Game', image: project1Image, description: 'In this JavaScript game you play as a baby dragon flying through the air and shooting down your enemies. This project started from a tutorial on FreeCodeCamp, but I branched off and made this project my own. This game is a work in progress, and you can test it out <a href="https://bphil1041.github.io/Javascript-game/" target="_blank" rel="noreferrer">on my GitHub</a>. For the best experience, please play the game on a computer in fullscreen.' },
-        { id: 2, name: 'Project 2', image: 'project2.jpg', description: 'Description of Project 2' },
-        { id: 3, name: 'Project 3', image: 'project3.jpg', description: 'Description of Project 3' },
+        { id: 2, name: 'myFlix (front end)', image: project2Image, description: 'The front end of a full-stack movie app called where you can learn about my favorite movies. Features include; sign up, login, logout, edit user info, delete account, filter movies by genre / director, and read individual movie info. <a href="https://myflix-bp.netlify.app/login" target="_blank" rel="noreferrer">  Click here </a> to check it out. ' },
+        { id: 3, name: 'myFlix (back end)', image: 'project3.jpg', description: 'Description of Project 3' },
         // Add more projects as needed
     ];
 
@@ -35,16 +36,16 @@ function Projects() {
                 ))}
             </Row>
             <Modal show={showModal} onHide={() => setShowModal(false)} centered>
-                <Modal.Header closeButton>
-                    <Modal.Title>{selectedProject?.name}</Modal.Title>
+                <Modal.Header>
+                    <Modal.Title className='modalTitle'>{selectedProject?.name}</Modal.Title>
                 </Modal.Header>
-                <Modal.Body>
+                <Modal.Body className='modalBody'>
                     <img src={selectedProject?.image} alt={selectedProject?.name} className="modal-image" />
                     <p dangerouslySetInnerHTML={{ __html: selectedProject?.description }}></p>
                     {/* Add more details about the project */}
                 </Modal.Body>
                 <Modal.Footer>
-                    <Button variant="secondary" onClick={() => setShowModal(false)}>Close</Button>
+                    <Button className='modalButton' variant="secondary" onClick={() => setShowModal(false)}>Close</Button>
                     {/* Add additional buttons or actions */}
                 </Modal.Footer>
             </Modal>
