e33f981023f5a7d33341e4eaa40e65905753aebd
bphil1041 <benjamin.r.phillips1041@gmail.com>
2024-03-17 10:58:05 -0500

    Update NavBar.js

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -6,11 +6,11 @@ import './NavBar.css'
 function NavBar() {
     return (
         <Navbar bg="dark" variant="dark" expand="lg">
-            <Navbar.Brand as={Link} to="/">Ben Phillips - Web Dev</Navbar.Brand>
+            <Navbar.Brand as={Link} to="/home">Ben Phillips - Web Dev</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="navbar-right"> {/* Use navbar-right class to push Nav items to the right */}
-                    <Nav.Link as={Link} to="/">Home</Nav.Link>
+                    <Nav.Link as={Link} to="/home">Home</Nav.Link>
                     <Nav.Link as={Link} to="/projects">Projects</Nav.Link>
                     <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
                 </Nav>
