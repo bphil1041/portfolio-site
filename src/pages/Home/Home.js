24a889e987ee2ee053580af1ee66ece282f22c20
bphil1041 <benjamin.r.phillips1041@gmail.com>
2024-04-23 14:06:49 -0500

    Update Home.js

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -8,9 +8,7 @@ function Home() {
             <h1 className='home-title'>Ben Phillips</h1>
             <p className='home-subtitle'>Web Developer / Musician</p>
             <img className='BenPhoto' src={BenPhoto}></img>
-            <p>Hello! My name is Ben. I'm a web developer and musician based in Chicago.</p>
-            <br></br>
-            <p>I view software development the same way I view music composition; as a ________ </p>
+            <p>Hello! My name is Ben. I'm a web developer and musician based in Chicago. As a lifelong musician, I have always loved the process of learning the universal language of music and using what I learn to develop my own voice</p>
         </div>
     );
 }
