0dce163c7af7de7d9351c43488f2917de5f7caa5
bphil1041 <benjamin.r.phillips1041@gmail.com>
2024-04-23 14:06:26 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,19 +6,26 @@ import Home from './pages/Home/Home';
 import Projects from './pages/Projects/Projects';
 import Contact from './pages/Contact/Contact';
 import Music from './pages/Music/Music';
+import ImageOne from './components/Parallax/ImageOne';
 
 function App() {
   return (
     <Router>
       <div className="App">
+
+
         <NavBar />
+
         <Routes>
           <Route path="/music" element={<Music />} />
           <Route path="/projects" element={<Projects />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/home" element={<Home />} />
+
         </Routes>
+
       </div>
+
     </Router>
   );
 }
