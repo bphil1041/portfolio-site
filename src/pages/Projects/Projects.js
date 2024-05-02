e1e7c5615a05949f681f4b895539959753ccd3a1
bphil1041 <benjamin.r.phillips1041@gmail.com>
2024-05-02 08:28:29 -0500

    Update Projects.js

--- src/pages/Projects/Projects.js
+++ src/pages/Projects/Projects.js
@@ -4,6 +4,7 @@ import DOMPurify from 'dompurify';
 import './Projects.css';
 import project1Image from '../../assets/Screenshot 2024-03-05 at 11.46.28 PM 2.png';
 import project2Image from '../../assets/myFlixScreenshot.png';
+import project3Image from '../../assets/movie_api_doc.png';
 import project4Image from '../../assets/Meet App.png';
 import project5Image from '../../assets/Gengar.png';
 
@@ -14,7 +15,7 @@ function Projects() {
     const projects = [
         { id: 1, name: 'JavaScript Game', image: project1Image, description: 'In this JavaScript game you play as a baby dragon flying through the air and shooting down your enemies. This project started from a tutorial on FreeCodeCamp, but I branched off and made this project my own. This game is a work in progress, and you can test it out <a href="https://bphil1041.github.io/Javascript-game/" target="_blank" rel="noreferrer">on my GitHub</a>. For the best experience, please play the game on a computer in fullscreen.' },
         { id: 2, name: 'myFlix (front end)', image: project2Image, description: 'The front end of a full-stack movie app called where you can learn about my favorite movies. Features include; sign up, login, logout, edit user info, delete account, filter movies by genre / director, and read individual movie info. <a href="https://myflix-bp.netlify.app/login" target="_blank" rel="noreferrer">  Click here </a> to check it out. ' },
-        { id: 3, name: 'myFlix (back end)', image: 'project3.jpg', description: 'Description of Project 3' },
+        { id: 3, name: 'myFlix (back end)', image: project3Image, description: 'Description of Project 3' },
         { id: 4, name: 'Meet App', image: project4Image, description: 'A serverless, progressive React Web App (PWA) using TDD technique. View the app <a href="https://bphil1041.github.io/meet-app/" target="_blank" rel="noreferrer"> here</a>' },
         { id: 5, name: 'Pokedex', image: project5Image, description: 'A simple Pokédex application that displays basic Pokémon information. Javascript, html, and CSS. View the app <a href="https://github.com/bphil1041/Pokedex" target="_blank" rel="noreferrer"> here</a>' },
 
