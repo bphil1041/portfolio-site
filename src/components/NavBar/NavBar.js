28d8b3d1c526b27f5dbbe9985d642f427f0cb436
bphil1041 <benjamin.r.phillips1041@gmail.com>
2024-05-06 15:50:16 -0500

    Update NavBar.js

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,21 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Navbar, Nav } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
-import './NavBar.css'
+
+import './NavBar.css';
 
 function NavBar() {
-    return (
+    const [activeLink, setActiveLink] = useState(null);
 
+    const handleNavLinkClick = (index) => {
+        setActiveLink(index);
+    };
+
+
+    return (
         <Navbar bg="transparent" variant="dark" expand="lg">
-            <Navbar.Brand as={Link} to="/home">Ben Phillips</Navbar.Brand>
+            <Navbar.Brand as={Link} to="/home" onClick={() => handleNavLinkClick(0)} className={activeLink === 0 ? 'active' : ''}>BP</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="navbar-right"> {/* Use navbar-right class to push Nav items to the right */}
-                    <Nav.Link as={Link} to="/home">Home</Nav.Link>
-                    <Nav.Link as={Link} to="/projects">Projects</Nav.Link>
-                    <Nav.Link as={Link} to="/music">Music</Nav.Link>
-                    <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
+                <Nav className="navbar-right">
+                    <Nav.Link as={Link} to="/home" onClick={() => handleNavLinkClick(1)} className={activeLink === 1 ? 'active' : ''}>Home</Nav.Link>
+                    <Nav.Link as={Link} to="/projects" onClick={() => handleNavLinkClick(2)} className={activeLink === 2 ? 'active' : ''}>Projects</Nav.Link>
+                    <Nav.Link as={Link} to="/music" onClick={() => handleNavLinkClick(3)} className={activeLink === 3 ? 'active' : ''}>Music</Nav.Link>
+                    <Nav.Link as={Link} to="/contact" onClick={() => handleNavLinkClick(4)} className={activeLink === 4 ? 'active' : ''}>Contact</Nav.Link>
                 </Nav>
             </Navbar.Collapse>
         </Navbar>
