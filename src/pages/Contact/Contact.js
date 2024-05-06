944a3e5892ae0873d92f1178535bb32ac3f1cb9b
bphil1041 <benjamin.r.phillips1041@gmail.com>
2024-05-06 15:50:25 -0500

    Update Contact.js

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,18 +1,30 @@
 import React from 'react';
+import { ReactComponent as LinkedinLogo } from '../../assets/LI-Bug.svg.original.svg';
+import { ReactComponent as GitHubLogo } from '../../assets/github-mark-white.svg';
 import './Contact.css';
 
 function Contact() {
     return (
         <div className="contact-container">
-            <h1>Contact Me</h1>
-            <h2>Want me to build your website or app? Or play trumpet for you?</h2>
-            <p>Email: benjamin.r.phillips1041@gmail.com</p>
+            <h1>Contact</h1>
+            <br></br>
+            <h2>Send business inquiries to:</h2>
+            <p><a href="mailto:benjamin.r.phillips1041@gmail.com">benjamin.r.phillips1041@gmail.com</a></p>
+            <br></br>
+            <br></br>
+            <h2>Socials:</h2>
+
             <p>
-                <a href="https://www.linkedin.com/in/ben-phillips-b6532027a/" target="_blank" rel="noreferrer" >My Linkedin Profile</a>
+                <a href="https://www.linkedin.com/in/ben-phillips-b6532027a/" target="_blank" rel="noreferrer">
+                    <LinkedinLogo />
+                </a>
             </p>
             <p>
-                <a href="https://github.com/bphil1041" target="_blank" rel="noreferrer" >My GitHub</a>
+                <a href="https://github.com/bphil1041" target="_blank" rel="noreferrer">
+                    <GitHubLogo />
+                </a>
             </p>
+
         </div>
     );
 }
