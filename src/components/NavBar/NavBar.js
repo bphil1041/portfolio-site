dcba3b556f58be883c47a0974849435843bee249
bphil1041 <benjamin.r.phillips1041@gmail.com>
2024-05-27 10:23:32 -0500

    Update NavBar.js

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,28 +1,25 @@
 import React, { useState } from 'react';
 import { Navbar, Nav } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
-
-
 import './NavBar.css';
 
-function NavBar() {
+function NavBar({ scrollToSection, refs }) {
     const [activeLink, setActiveLink] = useState(null);
 
-    const handleNavLinkClick = (index) => {
+    const handleNavLinkClick = (index, sectionRef) => {
         setActiveLink(index);
+        scrollToSection(sectionRef);
     };
-
 
     return (
         <Navbar className='navbar' bg="black" variant="dark" expand="lg">
-            <Navbar.Brand as={Link} to="/home" onClick={() => handleNavLinkClick(0)} className={activeLink === 0 ? 'active' : ''}>BP</Navbar.Brand>
+            <Navbar.Brand onClick={() => handleNavLinkClick(0, refs.homeRef)} className={activeLink === 0 ? 'active' : ''}>BP</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="navbar-right">
-                    <Nav.Link as={Link} to="/home" onClick={() => handleNavLinkClick(1)} className={activeLink === 1 ? 'active' : ''}>Home</Nav.Link>
-                    <Nav.Link as={Link} to="/projects" onClick={() => handleNavLinkClick(2)} className={activeLink === 2 ? 'active' : ''}>Projects</Nav.Link>
-                    <Nav.Link as={Link} to="/music" onClick={() => handleNavLinkClick(3)} className={activeLink === 3 ? 'active' : ''}>Music</Nav.Link>
-                    <Nav.Link as={Link} to="/contact" onClick={() => handleNavLinkClick(4)} className={activeLink === 4 ? 'active' : ''}>Contact</Nav.Link>
+                    <Nav.Link onClick={() => handleNavLinkClick(1, refs.homeRef)} className={activeLink === 1 ? 'active' : ''}>Home</Nav.Link>
+                    <Nav.Link onClick={() => handleNavLinkClick(2, refs.projectsRef)} className={activeLink === 2 ? 'active' : ''}>Projects</Nav.Link>
+                    <Nav.Link onClick={() => handleNavLinkClick(3, refs.musicRef)} className={activeLink === 3 ? 'active' : ''}>Music</Nav.Link>
+                    <Nav.Link onClick={() => handleNavLinkClick(4, refs.contactRef)} className={activeLink === 4 ? 'active' : ''}>Contact</Nav.Link>
                 </Nav>
             </Navbar.Collapse>
         </Navbar>
