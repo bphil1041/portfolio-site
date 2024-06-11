c77ddc9ab4939d6ae7d26fa1c16a25c8ccc78000
bphil1041 <benjamin.r.phillips1041@gmail.com>
2024-06-10 21:31:23 -0500

    Update Home.js

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -6,11 +6,10 @@ function Home() {
     return (
         <div>
             <h1 className='home-title'>Ben Phillips</h1>
-            <p className='home-subtitle'>Software Developer / Musician</p>
+            <p className='home-subtitle'>Full-Stack Web Developer / Musician</p>
             <img className='BenPhoto' src={BenPhoto}></img>
-            <p className='about-me'>Hello! My name is Ben. I am a software developer with a diverse background in music performance and composition, customer service, and sales in the bike industry. My unique blend of experiences has honed my communication, problem-solving, and creative thinking skills, making me adept at approaching software development with a holistic perspective.</p>
-            <p className='about-me'>My background in customer service and sales has greatly enhanced my ability to communicate effectively and solve problems. As I transitioned into software development, I specialized in front-end technologies like React and React Native, focusing on crafting engaging user experiences, implementing responsive designs, and ensuring accessibility standards.</p>
-            <p className='about-me'>I am passionate about merging my love for music with my expertise in technology. I am seeking opportunities that allow me to contribute my skills to innovative projects and collaborate with like-minded professionals. I invite you to connect with me to explore potential collaborations or discuss how my skills can benefit your team.</p>
+            <p className='about-me'>Hello! My name is Ben, a Full-Stack Web Developer with a diverse background in music performance and composition, customer service, and sales in the bike industry. These experiences have honed my communication, problem-solving, and creative thinking skills, enabling a holistic approach to software development. Specializing in front-end technologies like React and React Native, I focus on crafting engaging user experiences, responsive designs, and ensuring accessibility standards. I am passionate about merging my love for music with my expertise in technology and seek opportunities to contribute to innovative projects and collaborate with like-minded professionals. Let's connect to explore potential collaborations or discuss how my skills can benefit your team.</p>
+
         </div>
     );
 }
