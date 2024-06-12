d3efac28085290aad1af144c79b33e30fa267d77
bphil1041 <benjamin.r.phillips1041@gmail.com>
2024-06-11 22:07:25 -0500

    Create About.js

---
+++ src/pages/About/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './About.css';
+
+function About() {
+    return (
+        <div className='about-section'>
+            <h1 className='about-title'>About Me</h1>
+            <p className='about-content'>With a background in music performance and composition, I find the process of coding remarkably similar to composing music. Just as a composer carefully crafts melodies and harmonies, I meticulously build software solutions, ensuring each line of code contributes to a harmonious and functional whole. My experience in music has honed my ability to think creatively and solve problems in innovative ways. Whether it's orchestrating a complex application or composing a new piece of music, I approach both with the same passion and dedication to excellence. My goal is to merge my love for music with my expertise in technology, creating elegant and effective solutions that resonate with users and clients alike.</p>
+            <p className='about-content'>In addition to coding and music, I also enjoy Muay Thai, biking, cooking, tending to my houseplants, and spending time with my friends and family.</p>
+        </div>
+
+    );
+}
+
+export default About;
