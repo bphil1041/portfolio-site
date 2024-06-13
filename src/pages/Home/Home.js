44ece369a81636c3dd43e04d81420295e41c1250
bphil1041 <benjamin.r.phillips1041@gmail.com>
2024-06-13 01:09:23 -0500

    Update Home.js
    edit alt desc

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -7,7 +7,7 @@ function Home() {
         <div>
             <h1 className='home-title'>Ben Phillips</h1>
             <p className='home-subtitle'>Full-Stack Web Developer / Musician</p>
-            <img className='BenPhoto' src={BenPhoto}></img>
+            <img className='BenPhoto' alt='Ben Phillips playing the trumpet with blue and purple lighting.' src={BenPhoto}></img>
             <p className='hey'>Hello and welcome to my humble abode! I love to use my creative and problem solving skills to help people, whether that be with the keyboard or the trumpet. Please feel free to have a look around.</p>
         </div>
     );
