36db834ce5c6272f17d1111e655d189c838618e0
bphil1041 <benjamin.r.phillips1041@gmail.com>
2024-06-14 08:39:22 -0500

    Update Projects.js

--- src/pages/Projects/Projects.js
+++ src/pages/Projects/Projects.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Card, Col, Row, Modal, Button } from 'react-bootstrap';
 import DOMPurify from 'dompurify';
 import './Projects.css';
@@ -15,7 +15,7 @@ function Projects() {
     const [selectedProject, setSelectedProject] = React.useState(null);
 
     const projects = [
-        { id: 1, name: 'JavaScript Game', image: project1Image, description: 'In this JavaScript game you play as a baby dragon flying through the air and shooting down your enemies. This project started from a tutorial on FreeCodeCamp, but I branched off and made this project my own. This game is a work in progress, and you can test it out <a href="https://github.com/bphil1041/Javascript-game" target="_blank" rel="noreferrer">on my GitHub</a>. For the best experience, please play the game on a computer in fullscreen. <br> <br> Technologies used: JavaScript, HTML, CSS.' },
+        { id: 1, name: 'JavaScript Game', image: project1Image, description: 'In this JavaScript game you play as a baby dragon flying through the air and shooting down your enemies. This project started from a tutorial on FreeCodeCamp, but I branched off and made this project my own. This game is a work in progress, and you can test it out <a href="https://github.com/bphil1041/Javascript-game" target="_blank" rel="noreferrer" >on my GitHub</a>. For the best experience, please play the game on a computer in fullscreen. <br> <br> Technologies used: JavaScript, HTML, CSS.' },
         { id: 2, name: 'myFlix (front end)', image: project2Image, description: 'The front end of a full-stack movie app called where you can learn about my favorite movies. Features include; sign up, login, logout, edit user info, delete account, filter movies by genre / director, and read individual movie info. <a href="https://github.com/bphil1041/myFlix-client" target="_blank" rel="noreferrer">  Click here </a> to check it out. <br> <br> Technologies used: React, Bootstrap, Parcel. ' },
         { id: 3, name: 'myFlix (back end)', image: project3Image, description: 'This is the server-side for the myFlix app which provides users with information about movies, directors, and genres. Users can register, update their profiles, and create a list of favorite movies. <br> <br> Technologies used: Node.js, Express, REST API, MongoDB, JWT Authentication, Data Validation, and Heroku. <br> <br> View the code <a href="https://github.com/bphil1041/Movie-Api" target="_blank" rel="noreferrer"> on my GitHub </a>' },
         { id: 4, name: 'Meet App', image: project4Image, description: 'A serverless, progressive React Web App (PWA) using TDD technique. This app fetches and displays events fetched from a Google Calendar. <br> <br> Technologies used: React, Google Calendar API, AWS Lambda, Jest (Unit and Integration Testing), Jest Cucumber (User Acceptance Testing), Puppeteer, Atatus (Performance Monitoring). <br> <br> View the code <a href="https://github.com/bphil1041/meet-app" target="_blank" rel="noreferrer"> on my GitHub.</a>' },
@@ -29,6 +29,19 @@ function Projects() {
         setSelectedProject(project);
         setShowModal(true);
     };
+
+    useEffect(() => {
+        if (showModal) {
+            const links = document.querySelectorAll('.modalBody a');
+            links.forEach(link => {
+                link.addEventListener('click', (e) => {
+                    e.preventDefault();
+                    e.stopPropagation();
+                    window.open(link.href, '_blank', 'noopener,noreferrer');
+                });
+            });
+        }
+    }, [showModal]);
 
     return (
         <div className="projects-container">
