e205a8f69472b4c7dab110a0cf323607e6a03e27
bphil1041 <benjamin.r.phillips1041@gmail.com>
2024-06-16 21:16:29 -0500

    Update About.js

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import './About.css';
 
+const pdfFile = require('../../assets/Phillips-Benjamin-resume.pdf');
+
+
 function About() {
     return (
         <div className='about-section'>
@@ -8,7 +11,9 @@ function About() {
             <p className='about-content'>With a background in music performance and composition, I find coding similar to composing music. Just as a composer crafts melodies, I build software, ensuring each line of code contributes to a functional whole. My music experience hones my creative problem-solving skills. Whether orchestrating an application or composing music, I approach both with passion and dedication. My goal is to merge my love for music with my tech expertise, creating elegant and effective solutions for users and clients.</p>
             <p className='about-content'>I am also passionate about video games, which inspire my approach to coding. The intricate worlds and engaging mechanics of games fuel my creativity and drive to build user-friendly applications. Whether playing or coding, I bring the same enthusiasm and attention to detail, always striving to create innovative solutions that captivate and delight users.</p>
             <p className='about-content'>In addition to coding and music, I also enjoy Muay Thai, biking, cooking, tending to my houseplants, and spending time with my friends and family.</p>
+            <a href={pdfFile} download='Phillips-Benjamin-resume.pdf' className='download-link'>Download my resume!</a>
         </div>
+
 
     );
 }
