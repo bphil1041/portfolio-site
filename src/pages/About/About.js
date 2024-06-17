7cef11c8a18dd27fb86d8f2ca876ab5189212fc9
bphil1041 <benjamin.r.phillips1041@gmail.com>
2024-06-17 09:25:06 -0500

    Update About.js

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,5 +1,17 @@
 import React from 'react';
 import './About.css';
+import { FaReact } from "react-icons/fa";
+import { FaHtml5 } from "react-icons/fa";
+import { IoLogoJavascript } from "react-icons/io";
+import { FaCss3Alt } from "react-icons/fa";
+import { FaAngular } from "react-icons/fa";
+import { FaBootstrap } from "react-icons/fa";
+import { FaNodeJs } from "react-icons/fa";
+import { BsFiletypeScss } from "react-icons/bs";
+import { DiMongodb } from "react-icons/di";
+import { FaGithub } from "react-icons/fa";
+import { SiTypescript } from "react-icons/si";
+
 
 const pdfFile = require('../../assets/Phillips-Benjamin-resume.pdf');
 
@@ -12,6 +24,21 @@ function About() {
             <p className='about-content'>I am also passionate about video games, which inspire my approach to coding. The intricate worlds and engaging mechanics of games fuel my creativity and drive to build user-friendly applications. Whether playing or coding, I bring the same enthusiasm and attention to detail, always striving to create innovative solutions that captivate and delight users.</p>
             <p className='about-content'>In addition to coding and music, I also enjoy Muay Thai, biking, cooking, tending to my houseplants, and spending time with my friends and family.</p>
             <a href={pdfFile} download='Phillips-Benjamin-resume.pdf' className='download-link'>Download my resume!</a>
+            <br></br>
+            <p className='skills'>Skills</p>
+            <div className='skills-icons'>
+                <FaReact className='icon' /><IoLogoJavascript className='icon' />
+                <FaHtml5 className='icon' /><FaCss3Alt className='icon' /><BsFiletypeScss className='icon' />
+                <FaAngular className='icon' /><FaBootstrap className='icon' /><FaNodeJs className='icon' /><FaGithub className='icon' /><DiMongodb className='icon' />
+                <SiTypescript className='icon' />
+            </div>
+
+
+
+
+
+
+
         </div>
 
 
