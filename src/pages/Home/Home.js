9d1a1ccb9d27ff7a4b61c5cd93a9efcb0cca8f93
bphil1041 <benjamin.r.phillips1041@gmail.com>
2024-10-21 23:58:16 -0500

    noise experimentation

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,16 +2,30 @@ import React from 'react';
 import './Home.css';
 import BenPhoto from '../../assets/IMG_0346 2.png';
 
+// Import the sound file
+import dankNoise from '../../assets/dank_noise.wav'; 
+
 function Home({ scrollToSection, aboutRef }) {
+    const playSound = () => {
+        const sound = document.getElementById('dank-noise');
+        sound.currentTime = 0; // Reset the sound to the beginning
+        sound.play();
+        scrollToSection(aboutRef); // Trigger scroll after playing sound
+    };
+
     return (
         <div className='fade-in'>
             <h1 className='home-title'>Ben Phillips</h1>
             <p className='home-subtitle'>Full-Stack Web Developer / Musician</p>
             <img className='BenPhoto' alt='Ben Phillips playing the trumpet with blue and purple lighting.' src={BenPhoto}></img>
-            {/* Animated arrow with click functionality */}
-            <div className="scroll-down" onClick={() => scrollToSection(aboutRef)}>
+
+            {/* Trigger scroll on click */}
+            <div className="scroll-down" onClick={playSound}>
                 &#x2193; {/* Down arrow */}
             </div>
+
+            {/* Sound Element */}
+            <audio id="dank-noise" src={dankNoise} />
         </div>
     );
 }
--- src/pages/Projects/Projects.js
+++ src/pages/Projects/Projects.js
@@ -12,29 +12,30 @@ import project7Image from '../../assets/myflix-angular-screenshot.png';
 import project8Image from '../../assets/recipease-image.png';
 import project9Image from '../../assets/ColorChord-screenshot.png';
 
+// Import sound files
+import openSound from '../../assets/space_ui_edit.wav';  // Changed to open sound
+import closeSound from '../../assets/06_ui_space_toggle_off.wav';  // Changed to close sound
 
 function Projects() {
     const [showModal, setShowModal] = React.useState(false);
     const [selectedProject, setSelectedProject] = React.useState(null);
 
     const projects = [
-
-        { id: 7, name: 'myFlix Angular (front end)', image: project7Image, description: 'The front end of a full-stack movie app called myFlix, build using Angular, where you can learn about my favorite movies. Features include; sign up, login, logout, edit user info, delete account, filter movies by genre / director, and read individual movie info. <a href="https://github.com/bphil1041/myFlix-Angular-client" target="_blank" rel="noreferrer">  Click here </a> to check it out. <br> <br> Technologies used: Angular, TypeScript, HTML, CSS, SCSS ' },
-        { id: 2, name: 'myFlix React (front end)', image: project2Image, description: 'The front end of a full-stack movie app called myFlix, built using React, where you can learn about my favorite movies. Features include; sign up, login, logout, edit user info, delete account, filter movies by genre / director, and read individual movie info. <a href="https://github.com/bphil1041/myFlix-client" target="_blank" rel="noreferrer">  Click here </a> to check it out. <br> <br> Technologies used: React, Bootstrap, Parcel. ' },
-        { id: 3, name: 'myFlix (back end)', image: project3Image, description: 'This is the server-side for the myFlix app which provides users with information about movies, directors, and genres. Users can register, update their profiles, and create a list of favorite movies. <br> <br> Technologies used: Node.js, Express, REST API, MongoDB, JWT Authentication, Data Validation, and Heroku. <br> <br> View the code <a href="https://github.com/bphil1041/Movie-Api" target="_blank" rel="noreferrer"> on my GitHub </a>' },
-        { id: 8, name: 'RecipEase', image: project8Image, description: 'A Django App for viewing and creating recipes. Users can sign up, log in, log out, view a list of recipes, view recipe details, create new recipes, view profile information, and change password. View the cod <a href="https://github.com/bphil1041/recipe-app" target=_blank" ref=noreferrer"> on my Github</a>' },
-        { id: 4, name: 'Meet App', image: project4Image, description: 'A serverless, progressive React Web App (PWA) using TDD technique. This app fetches and displays events fetched from a Google Calendar. <br> <br> Technologies used: React, Google Calendar API, AWS Lambda, Jest (Unit and Integration Testing), Jest Cucumber (User Acceptance Testing), Puppeteer, Atatus (Performance Monitoring). <br> <br> View the code <a href="https://github.com/bphil1041/meet-app" target="_blank" rel="noreferrer"> on my GitHub.</a>' },
-        { id: 5, name: 'Pokedex', image: project5Image, description: 'A simple Pokédex application that displays basic Pokémon information that is fetched from an API.<br> <br> Technologies used: <br> Javascript, HTML, and CSS. <br> <br> View the code <a href="https://github.com/bphil1041/Pokedex" target="_blank" rel="noreferrer"> on my GitHub.</a>' },
-        { id: 6, name: 'Chat App', image: project6Image, description: 'A React Native Chat app that is designed to facilitate communcation between users. Users can share text messages, images, and their current location. <br> <br> View the code <a href="https://github.com/bphil1041/chat-demo" target="_blank" rel="noreferrer">on my GitHub</a>' },
-        { id: 1, name: 'JavaScript Game', image: project1Image, description: 'In this JavaScript game you play as a baby dragon flying through the air and shooting down your enemies. This project started from a tutorial on FreeCodeCamp, but I branched off and made this project my own. This game is a work in progress, and you can test it out <a href="https://github.com/bphil1041/Javascript-game" target="_blank" rel="noreferrer" >on my GitHub</a>. For the best experience, please play the game on a computer in fullscreen. <br> <br> Technologies used: JavaScript, HTML, CSS.' },
-        { id: 9, name: 'ColorChord', image: project9Image, description: 'This project is a work in progress! The goal is to build a painting / music app where each brushtroke plays a note. There are twelve colors to choose from that correspond with all twelve pitches of Western music. The Front-End is built using React, Konva.js for managing the drawing canvas, Tone.js for sound generation and linking colors to musical notes, and Web Audio API for sound processing and playback. Follow along <a href="https://github.com/bphil1041/ColorChord" target="_blank" rel="noreferrer"> on my GitHub </a>  as I make progress!' },
-
-        // Add more projects as needed
+        { id: 7, name: 'myFlix Angular (front end)', image: project7Image, description: 'The front end of a full-stack movie app called myFlix, built using Angular, where you can learn about my favorite movies. Features include; sign up, login, logout, edit user info, delete account, filter movies by genre / director, and read individual movie info. <a href="https://github.com/bphil1041/myFlix-Angular-client" target="_blank" rel="noreferrer">Click here</a> to check it out. <br /><br /> Technologies used: Angular, TypeScript, HTML, CSS, SCSS ' },
+        { id: 2, name: 'myFlix React (front end)', image: project2Image, description: 'The front end of a full-stack movie app called myFlix, built using React, where you can learn about my favorite movies. Features include; sign up, login, logout, edit user info, delete account, filter movies by genre / director, and read individual movie info. <a href="https://github.com/bphil1041/myFlix-client" target="_blank" rel="noreferrer">Click here</a> to check it out. <br /><br /> Technologies used: React, Bootstrap, Parcel. ' },
+        { id: 3, name: 'myFlix (back end)', image: project3Image, description: 'This is the server-side for the myFlix app which provides users with information about movies, directors, and genres. Users can register, update their profiles, and create a list of favorite movies. <br /><br /> Technologies used: Node.js, Express, REST API, MongoDB, JWT Authentication, Data Validation, and Heroku. <br /><br /> View the code <a href="https://github.com/bphil1041/Movie-Api" target="_blank" rel="noreferrer">on my GitHub</a>' },
+        { id: 8, name: 'RecipEase', image: project8Image, description: 'A Django App for viewing and creating recipes. Users can sign up, log in, log out, view a list of recipes, view recipe details, create new recipes, view profile information, and change password. View the code <a href="https://github.com/bphil1041/recipe-app" target="_blank" rel="noreferrer">on my Github</a>' },
+        { id: 4, name: 'Meet App', image: project4Image, description: 'A serverless, progressive React Web App (PWA) using TDD technique. This app fetches and displays events fetched from a Google Calendar. <br /><br /> Technologies used: React, Google Calendar API, AWS Lambda, Jest (Unit and Integration Testing), Jest Cucumber (User Acceptance Testing), Puppeteer, Atatus (Performance Monitoring). <br /><br /> View the code <a href="https://github.com/bphil1041/meet-app" target="_blank" rel="noreferrer">on my GitHub.</a>' },
+        { id: 5, name: 'Pokedex', image: project5Image, description: 'A simple Pokédex application that displays basic Pokémon information that is fetched from an API.<br /><br /> Technologies used: <br />Javascript, HTML, and CSS. <br /><br /> View the code <a href="https://github.com/bphil1041/Pokedex" target="_blank" rel="noreferrer">on my GitHub.</a>' },
+        { id: 6, name: 'Chat App', image: project6Image, description: 'A React Native Chat app that is designed to facilitate communication between users. Users can share text messages, images, and their current location. <br /><br /> View the code <a href="https://github.com/bphil1041/chat-demo" target="_blank" rel="noreferrer">on my GitHub</a>' },
+        { id: 1, name: 'JavaScript Game', image: project1Image, description: 'In this JavaScript game you play as a baby dragon flying through the air and shooting down your enemies. This project started from a tutorial on FreeCodeCamp, but I branched off and made this project my own. This game is a work in progress, and you can test it out <a href="https://github.com/bphil1041/Javascript-game" target="_blank" rel="noreferrer">on my GitHub</a>. For the best experience, please play the game on a computer in fullscreen. <br /><br /> Technologies used: JavaScript, HTML, CSS.' },
+        { id: 9, name: 'ColorChord', image: project9Image, description: 'This project is a work in progress! The goal is to build a painting/music app where each brushstroke plays a note. There are twelve colors to choose from that correspond with all twelve pitches of Western music. The Front-End is built using React, Konva.js for managing the drawing canvas, Tone.js for sound generation and linking colors to musical notes, and Web Audio API for sound processing and playback. Follow along <a href="https://github.com/bphil1041/ColorChord" target="_blank" rel="noreferrer">on my GitHub</a> as I make progress!' },
     ];
 
     const handleProjectClick = (project) => {
         setSelectedProject(project);
         setShowModal(true);
+        document.getElementById('open-sound').play(); // Play the open sound
     };
 
     useEffect(() => {
@@ -49,6 +50,11 @@ function Projects() {
             });
         }
     }, [showModal]);
+
+    const handleCloseModal = () => {
+        document.getElementById('close-sound').play(); // Play the close sound
+        setShowModal(false);
+    };
 
     return (
         <div className="projects-container">
@@ -66,8 +72,8 @@ function Projects() {
                     </Col>
                 ))}
             </Row>
-            <Modal show={showModal} onHide={() => setShowModal(false)} centered>
-                <Modal.Header>
+            <Modal show={showModal} onHide={handleCloseModal} centered>
+                <Modal.Header closeButton onHide={handleCloseModal}>
                     <Modal.Title className='modalTitle'>{selectedProject?.name}</Modal.Title>
                 </Modal.Header>
                 <Modal.Body className='modalBody'>
@@ -75,9 +81,12 @@ function Projects() {
                     <p dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(selectedProject?.description) }}></p>
                 </Modal.Body>
                 <Modal.Footer>
-                    <Button className='modalButton' variant="secondary" onClick={() => setShowModal(false)}>Close</Button>
+                    <Button className='modalButton' variant="secondary" onClick={handleCloseModal}>Close</Button>
                 </Modal.Footer>
             </Modal>
+            {/* Sound Elements */}
+            <audio id="open-sound" src={openSound} />
+            <audio id="close-sound" src={closeSound} />
         </div>
     );
 }
