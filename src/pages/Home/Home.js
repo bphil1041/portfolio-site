66e7ccb58d4687955349a7f10df88d83e25ebb7d
bphil1041 <benjamin.r.phillips1041@gmail.com>
2024-11-02 21:04:25 -0500

    Update Home.js
    ensure sound consistency

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,34 +1,33 @@
-import React from 'react';
-import './Home.css';
-import BenPhoto from '../../assets/IMG_0346 2.png';
-
-// Import the sound file
-import dankNoise from '../../assets/dank_noise.wav'; 
+import React from "react";
+import "./Home.css";
+import BenPhoto from "../../assets/IMG_0346 2.png";
+import { useSound } from "../../context/SoundContext";
 
 function Home({ scrollToSection, aboutRef }) {
-    const playSound = () => {
-        const sound = document.getElementById('dank-noise');
-        sound.currentTime = 0; // Reset the sound to the beginning
-        sound.play();
-        scrollToSection(aboutRef); // Trigger scroll after playing sound
-    };
+  const { playDankNoise } = useSound();
 
-    return (
-        <div className='fade-in'>
-            <h1 className='home-title'>Ben Phillips</h1>
-            <p className='home-subtitle'>Full-Stack Web Developer / Musician</p>
-            <p className='home-location'>Chicago, IL</p>
-            <img className='BenPhoto' alt='Ben Phillips playing the trumpet with blue and purple lighting.' src={BenPhoto}></img>
+  const playSoundAndScroll = () => {
+    playDankNoise();
+    scrollToSection(aboutRef);
+  };
 
-            {/* Trigger scroll on click */}
-            <div className="scroll-down" onClick={playSound}>
-                &#x2193; {/* Down arrow */}
-            </div>
+  return (
+    <div className="fade-in">
+      <h1 className="home-title">Ben Phillips</h1>
+      <p className="home-subtitle">Full-Stack Web Developer / Musician</p>
+      <p className="home-location">Chicago, IL</p>
+      <img
+        className="BenPhoto"
+        alt="Ben Phillips playing the trumpet with blue and purple lighting."
+        src={BenPhoto}
+      />
 
-            {/* Sound Element */}
-            <audio id="dank-noise" src={dankNoise} />
-        </div>
-    );
+      {/* Trigger scroll on click */}
+      <div className="scroll-down" onClick={playSoundAndScroll}>
+        &#x2193; {/* Down arrow */}
+      </div>
+    </div>
+  );
 }
 
 export default Home;
