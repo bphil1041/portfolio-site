bba2cd9a3b8dfc0dc76a521e33bc23039b13f827
bphil1041 <benjamin.r.phillips1041@gmail.com>
2024-11-03 19:41:24 -0600

    edit layout

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -30,57 +30,60 @@ function About() {
   };
 
   return (
-    <div className="about-section">
-      <h1 className="about-title">About Me</h1>
-      <p className="about-content">
-        With a background in music performance and composition, I find coding
-        similar to composing music. Just as a composer crafts melodies, I build
-        software, ensuring each line of code contributes to a functional whole.
-        My music experience hones my creative problem-solving skills. Whether
-        orchestrating an application or composing music, I approach both with
-        passion and dedication. My goal is to merge my love for music with my
-        tech expertise, creating elegant and effective solutions for users and
-        clients.
-      </p>
-      <p className="about-content">
-        I am also passionate about video games, which inspire my approach to
-        coding. The intricate worlds and engaging mechanics of games fuel my
-        creativity and drive to build user-friendly applications. Whether
-        playing or coding, I bring the same enthusiasm and attention to detail,
-        always striving to create innovative solutions that captivate and
-        delight users.
-      </p>
-      <p className="about-content">
-        In addition to coding and music, I also enjoy Muay Thai, biking,
-        cooking, tending to my houseplants, and spending time with my friends
-        and family.
-      </p>
+    <div className="about-skills-container">
+      <div className="about-section">
+        <h1 className="about-title">About Me</h1>
+        <p className="about-content">
+          With a background in music performance and composition, I find coding
+          similar to composing music. Just as a composer crafts melodies, I
+          build software, ensuring each line of code contributes to a functional
+          whole. My music experience hones my creative problem-solving skills.
+          Whether orchestrating an application or composing music, I approach
+          both with passion and dedication. My goal is to merge my love for
+          music with my tech expertise, creating elegant and effective solutions
+          for users and clients.
+        </p>
+        <p className="about-content">
+          I am also passionate about video games, which inspire my approach to
+          coding. The intricate worlds and engaging mechanics of games fuel my
+          creativity and drive to build user-friendly applications. Whether
+          playing or coding, I bring the same enthusiasm and attention to
+          detail, always striving to create innovative solutions that captivate
+          and delight users.
+        </p>
+        <p className="about-content">
+          In addition to coding and music, I also enjoy Muay Thai, biking,
+          cooking, tending to my houseplants, and spending time with my friends
+          and family.
+        </p>
 
-      <a
-        href={pdfFile}
-        download="Phillips-Benjamin-resume.pdf"
-        className="download-link"
-        onClick={handleDownloadClick}
-      >
-        Download my resume!
-      </a>
+        <a
+          href={pdfFile}
+          download="Phillips-Benjamin-resume.pdf"
+          className="download-link"
+          onClick={handleDownloadClick}
+        >
+          Download my resume!
+        </a>
+      </div>
 
-      <br />
-      <p className="skills">Skills</p>
-      <div className="skills-icons">
-        <FaReact className="icon" />
-        <IoLogoJavascript className="icon" />
-        <FaHtml5 className="icon" />
-        <FaCss3Alt className="icon" />
-        <BsFiletypeScss className="icon" />
-        <FaAngular className="icon" />
-        <FaBootstrap className="icon" />
-        <FaNodeJs className="icon" />
-        <FaGithub className="icon" />
-        <DiMongodb className="icon" />
-        <SiTypescript className="icon" />
-        <FaPython className="icon" />
-        <DiDjango className="icon" />
+      <div className="skills-section">
+        <p className="skills">Skills</p>
+        <div className="skills-icons">
+          <FaReact className="icon" />
+          <IoLogoJavascript className="icon" />
+          <FaHtml5 className="icon" />
+          <FaCss3Alt className="icon" />
+          <BsFiletypeScss className="icon" />
+          <FaAngular className="icon" />
+          <FaBootstrap className="icon" />
+          <FaNodeJs className="icon" />
+          <FaGithub className="icon" />
+          <DiMongodb className="icon" />
+          <SiTypescript className="icon" />
+          <FaPython className="icon" />
+          <DiDjango className="icon" />
+        </div>
       </div>
     </div>
   );
